3d93269a898f912535bf2effa886baddb07f027b
flyrk <simon_xiameng@126.com>
2017-09-05 15:02:15 +0800

    Add CommentInputBox style

--- src/components/Comments/CommentInputBox/index.js
+++ src/components/Comments/CommentInputBox/index.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import './index.css';
 
 export default class CommentInputBox extends Component {
   static propTypes = {
@@ -8,8 +9,14 @@ export default class CommentInputBox extends Component {
 
   render() {
     return (
-      <div className="comment-inputbox-wrap">
-        
+      <div className="comment-inputbox-wrap box">
+        <div className="field">
+          <div className="control">
+            <input className="input" type="text" placeholder="你的名字" />
+            <textarea className="textarea" type="text" placeholder="请输入留言..." rows="10" />
+          </div>
+        </div>
+        <button className="button">评论</button>
       </div>
     )
   }
