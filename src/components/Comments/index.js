beaf55033eec6525e134ae922ea636014f741ade
flyrk <simon_xiameng@126.com>
2017-09-10 16:35:25 +0800

    Change Comment structure & Add commentBox

--- src/components/Comments/index.js
+++ src/components/Comments/index.js
@@ -1,31 +1,87 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
-
-import CommentInputBox from './CommentInputBox';
-import CommentBox from './CommentBox';
+import { addComment } from '../../actions/commentActions';
 
 import './index.css';
 
 class Comments extends Component {
   static propTypes = {
-    prop: PropTypes
+    addComment: PropTypes.func.isRequired,
+    comments: PropTypes.array.isRequired
   };
 
   constructor(props) {
     super(props);
+    this.state = {
+      comments: [],
+      username: '',
+      comment: ''
+    };
+    this.handleOnChange = this.handleOnChange.bind(this);
+    this.handleOnClick = this.handleOnClick.bind(this);
+  }
+
+  handleOnChange(event) {
+    this.setState({ [event.target.name]: event.target.value });
+  }
+
+  handleOnClick() {
+    this.props.addComment(this.state);
+    this.setState({
+      comment: '',
+      comments: this.props.comments
+    });
   }
 
   render() {
+    const comments = this.state.comments;
+    const commentBox = (
+      <div className="commentbox-wrap box">
+        {comments.map((eachComment, id) => (
+          <div className="card" key={id}>
+            <header className="card-header">
+              <p className="card-header-title">{eachComment.username}:</p>
+            </header>
+            <div className="card-content">
+              <p className="content">{eachComment.comment}</p>
+            </div>
+          </div>
+        ))}
+      </div>);
     return (
       <div className="comments-container">
         <div className="comments-wrap">
-          <CommentInputBox />
-          <CommentBox />
+          <div className="comment-inputbox-wrap box">
+            <div className="field">
+              <div className="control">
+                <input className="input" type="text"
+                  name="username"
+                  placeholder="你的名字"
+                  onChange={this.handleOnChange}
+                  value={this.state.username}
+                />
+                <textarea className="textarea" type="text"
+                  name="comment"
+                  placeholder="请输入留言..." rows="10"
+                  onChange={this.handleOnChange}
+                  value={this.state.comment}
+                />
+              </div>
+            </div>
+            <button className="button" onClick={this.handleOnClick}>评论</button>
+          </div>
+          {this.state.comments.length !== 0 ? commentBox : ''}
+          
         </div>
       </div>
     )
   }
 }
 
-export default connect()(Comments);
+function mapStateProps(state) {
+  return {
+    comments: state.comments
+  }
+}
+export default connect(mapStateProps, { addComment })(Comments);
--- src/reducers/comments.js
+++ src/reducers/comments.js
@@ -4,9 +4,8 @@ const initialState = [];
 export default function(state = initialState, action={}) {
   switch(action.type) {
     case ADD_COMMENT:
-      return [
-        ...action.comment
-      ];
+      state.push(action.comment);
+      return state;
     default: 
       return state;
   }
